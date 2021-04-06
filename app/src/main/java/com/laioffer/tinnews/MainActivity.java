ec9127bd68f3324dae8388d97a70effde62a224e
Xinhao Song <songxinhao@hotmail.com>
2021-04-06 00:22:40 -0500

    Remove the top action bar for the whole app

--- app/src/main/java/com/laioffer/tinnews/MainActivity.java
+++ app/src/main/java/com/laioffer/tinnews/MainActivity.java
@@ -37,9 +37,8 @@ public class MainActivity extends AppCompatActivity {
                 getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
         navController = navHostFragment.getNavController();
         NavigationUI.setupWithNavController(navView, navController);
-        NavigationUI.setupActionBarWithNavController(this, navController);
-
-
+        // NavigationUI.setupActionBarWithNavController(this, navController);
+        NewsApi api = RetrofitClient.newInstance(this).create(NewsApi.class);
     }
 
     @Override
